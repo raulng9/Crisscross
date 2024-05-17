f47709a0a342e4f9ff76ec485bce9f6e4b6094d4
Raúl Núñez García <rnuneg01@estudiantes.unileon.es>
2024-05-18 00:58:18 +0200

    Light cleanup and refactor, including Venn library and basic drawing of intersection (not fully working yet)

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-
 const PATH_MOVIE_SEARCH_1 = "http://localhost:8000/example_movie_search_1.json";
 const PATH_MOVIE_SEARCH_2 = "http://localhost:8000/example_movie_search_2.json";
 const PATH_MOVIE_CAST_1 = "http://localhost:8000/example_movie_cast_1.json";
@@ -7,19 +5,16 @@ const PATH_MOVIE_CAST_2 = "http://localhost:8000/example_movie_cast_2.json";
 
 let castFirstMovie = [];
 let castSecondMovie = [];
-let commonMembers = [];
 
 async function fetchMoviesJSON(pathAPI) {
   const response = await fetch(pathAPI);
   const movies = await response.json();
-  console.log(movies.results[0]);
   return movies.results[0];
 }
 
 async function fetchCastJSON(pathAPI, movieId) {
   const response = await fetch(pathAPI);
   const cast = await response.json();
-  console.log(cast);
   return cast;
 }
 
@@ -34,39 +29,26 @@ Promise.all([
 ])
   .then(() => getIntersectingCast());
 
-
 function getIntersectingCast() {
   let idsCastFirstMovie = castFirstMovie.map(castMember => castMember.id);
   let idsCastSecondMovie = castSecondMovie.map(castMember => castMember.id);
 
   let intersectingIds = idsCastFirstMovie.filter(value => idsCastSecondMovie.includes(value));
 
-  commonMembers = castFirstMovie
+  let commonMembers = castFirstMovie
     .filter(castMember => intersectingIds.includes(castMember.id))
     .map(filteredCastMembers => filteredCastMembers.name);
 
-  console.log(commonMembers);
-
-  drawCircle(castFirstMovie);
-  drawCircle(castSecondMovie);
+  drawVennDiagram(commonMembers);
 }
 
-function drawCircle(dataFromMovie) {
-  var circleSvg = d3.select("#viz")
-    .append("svg")
-    .attr("width", dataFromMovie.length * 4)
-    .attr("height", dataFromMovie.length * 4);
+function drawVennDiagram(commonMembers) {
+  var sets = [{ sets: [castFirstMovie.map(member => member.name)], size: castFirstMovie.length },
+  { sets: [castSecondMovie.map(member => member.name)], size: castSecondMovie.length },
+  { sets: [commonMembers], size: commonMembers.length }];
 
-  console.log('drawing for ' + dataFromMovie.length);
+  console.log(sets);
 
-  circleSvg.append('circle')
-    .attr('cx', dataFromMovie.length * 2)
-    .attr('cy', dataFromMovie.length * 2)
-    .attr('r', dataFromMovie.length)
-    .attr('stroke', 'black')
-    .attr('fill', '#69a3b2');
-
-  circleSvg.append("text")
-    .attr("dy", 40)
-    .text(() => dataFromMovie.map(castMember => castMember.name));
+  var chart = venn.VennDiagram()
+  d3.select("#viz").datum(sets).call(chart);
 }
